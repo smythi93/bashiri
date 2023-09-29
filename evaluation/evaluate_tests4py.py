efce26a77b1fcdd80f7c3a64c817f3650307d12f
Marius Smytzek <marius.smytzek@cispa.de>
2023-09-29 11:33:21 +0200

    updated eval

--- evaluation/evaluate_tests4py.py
+++ evaluation/evaluate_tests4py.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from pathlib import Path
 from typing import Tuple, List
 
@@ -23,7 +24,7 @@ NEW_CONFUSION = f"{CONFUSION}_new"
 
 
 def get_results(
-    path: Path,
+    path: Path, refinement: bool = False
 ) -> Tuple[Confusion, Confusion, Confusion, List[float], List[float], List[float]]:
     results = Confusion(total=0)
     results_refinement = Confusion(total=0)
@@ -39,29 +40,31 @@ def get_results(
             precisions.append(tmp_results.precision_bug())
             recalls.append(tmp_results.recall_bug())
             results += tmp_results
-            for refinement in refactory_results[config][REFINEMENT]:
-                results_refinement += get_confusion(
-                    refactory_results[config][REFINEMENT][refinement],
-                    f"{path}_{config}_{REFINEMENT}_{refinement}",
-                )
-                if (
-                    NEW_EVAL in refactory_results[config][REFINEMENT][refinement]
-                    and NEW_CONFUSION
-                    in refactory_results[config][REFINEMENT][refinement]
-                ):
-                    results_new_tests += get_confusion(
-                        {
-                            EVAL: refactory_results[config][REFINEMENT][refinement][
-                                NEW_EVAL
-                            ],
-                            CONFUSION: refactory_results[config][REFINEMENT][
-                                refinement
-                            ][NEW_CONFUSION],
-                            TIME: 0,
-                        },
-                        f"{path}_{config}_new_{refinement}",
-                        exclude_no_eval=False,
+            if refinement:
+                for refinement_conf in refactory_results[config][REFINEMENT]:
+                    results_refinement += get_confusion(
+                        refactory_results[config][REFINEMENT][refinement_conf],
+                        f"{path}_{config}_{REFINEMENT}_{refinement}",
                     )
+                    if (
+                        NEW_EVAL
+                        in refactory_results[config][REFINEMENT][refinement_conf]
+                        and NEW_CONFUSION
+                        in refactory_results[config][REFINEMENT][refinement_conf]
+                    ):
+                        results_new_tests += get_confusion(
+                            {
+                                EVAL: refactory_results[config][REFINEMENT][
+                                    refinement_conf
+                                ][NEW_EVAL],
+                                CONFUSION: refactory_results[config][REFINEMENT][
+                                    refinement_conf
+                                ][NEW_CONFUSION],
+                                TIME: 0,
+                            },
+                            f"{path}_{config}_new_{refinement}",
+                            exclude_no_eval=False,
+                        )
     return (
         results,
         results_refinement,
@@ -72,13 +75,14 @@ def get_results(
     )
 
 
-def main():
+def main(refinement: bool = False):
     results = Confusion(total=0)
     results_refinement = Confusion(total=0)
     results_new_tests = Confusion(total=0)
     accuracies = dict()
     precisions = dict()
     recalls = dict()
+    per_subject = dict()
     for path in [
         MIDDLE_1,
         FASTAPI_1,
@@ -89,7 +93,10 @@ def main():
         COOKIECUTTER_2,
         FASTAPI_2,
     ]:
-        r, rr, rnt, as_, ps_, rs_ = get_results(path)
+        if refinement:
+            parts = path.parts
+            path = Path(*parts[:-1], parts[-1].replace(".json", "_refinement.json"))
+        r, rr, rnt, as_, ps_, rs_ = get_results(path, refinement=refinement)
         results += r
         results_refinement += rr
         results_new_tests += rnt
@@ -97,66 +104,81 @@ def main():
         accuracies[name] = as_
         precisions[name] = ps_
         recalls[name] = rs_
+        per_subject[name] = r, rr
 
     print("Results:")
     results.print()
-    print()
-    print("Results refinement:")
-    results_refinement.print()
-    print()
-    print("Results new tests:")
-    results_new_tests.print()
-    as_labels, as_values = zip(
-        *[(name.replace("_", "."), accuracies[name]) for name in accuracies]
-    )
-    ps_labels, ps_values = zip(
-        *[(name.replace("_", "."), precisions[name]) for name in precisions]
-    )
-    rs_labels, rs_values = zip(
-        *[(name.replace("_", "."), recalls[name]) for name in recalls]
-    )
-    size = 14
-    plt.rc("xtick", labelsize=size)
-    plt.rc("ytick", labelsize=size)
-    plt.figure(figsize=(8, 5))
-    plt.boxplot(as_values)
-    plt.xlabel("Subject", fontsize=size)
-    plt.xticks(
-        list(range(1, len(as_labels) + 1)),
-        as_labels,
-        rotation=-30,
-        ha="left",
-        rotation_mode="anchor",
-    )
-    plt.ylabel("Accuracy", fontsize=size)
-    plt.tight_layout()
-    plt.savefig("accuracies.pdf")
-    plt.clf()
-    plt.boxplot(ps_values)
-    plt.xlabel("Subject", fontsize=size)
-    plt.xticks(
-        list(range(1, len(ps_labels) + 1)),
-        ps_labels,
-        rotation=-30,
-        ha="left",
-        rotation_mode="anchor",
-    )
-    plt.ylabel("Precision", fontsize=size)
-    plt.savefig("precisions.pdf")
-    plt.clf()
-    plt.boxplot(rs_values)
-    plt.xlabel("Subject", fontsize=size)
-    plt.xticks(
-        list(range(1, len(rs_labels) + 1)),
-        rs_labels,
-        rotation=-30,
-        ha="left",
-        rotation_mode="anchor",
-    )
-    plt.ylabel("Recall", fontsize=size)
-    plt.savefig("recalls.pdf")
-    plt.clf()
+    if refinement:
+        print()
+        print("Results refinement:")
+        results_refinement.print()
+        print()
+        print("Results new tests:")
+        results_new_tests.print()
+        for n in per_subject:
+            r, rr = per_subject[n]
+            print()
+            print(f"{n}: {r.accuracy():.1f} -- {rr.accuracy():.1f} ")
+            print(
+                f"{n}: {r.average_macro_precision():.2f} -- {rr.average_macro_precision():.2f} "
+            )
+            print(
+                f"{n}: {r.average_macro_recall():.2f} -- {rr.average_macro_recall():.2f} "
+            )
+            print(f"{n}: {r.average_macro_f1():.2f} -- {rr.average_macro_f1():.2f} ")
+            print(f"{n}: {r.avg_time():.1f} -- {rr.avg_time():.1f} ")
+    else:
+        as_labels, as_values = zip(
+            *[(name.replace("_", "."), accuracies[name]) for name in accuracies]
+        )
+        ps_labels, ps_values = zip(
+            *[(name.replace("_", "."), precisions[name]) for name in precisions]
+        )
+        rs_labels, rs_values = zip(
+            *[(name.replace("_", "."), recalls[name]) for name in recalls]
+        )
+        size = 14
+        plt.rc("xtick", labelsize=size)
+        plt.rc("ytick", labelsize=size)
+        plt.figure(figsize=(8, 5))
+        plt.boxplot(as_values)
+        plt.xlabel("Subject", fontsize=size)
+        plt.xticks(
+            list(range(1, len(as_labels) + 1)),
+            as_labels,
+            rotation=-30,
+            ha="left",
+            rotation_mode="anchor",
+        )
+        plt.ylabel("Accuracy", fontsize=size)
+        plt.tight_layout()
+        plt.savefig("accuracies.pdf")
+        plt.clf()
+        plt.boxplot(ps_values)
+        plt.xlabel("Subject", fontsize=size)
+        plt.xticks(
+            list(range(1, len(ps_labels) + 1)),
+            ps_labels,
+            rotation=-30,
+            ha="left",
+            rotation_mode="anchor",
+        )
+        plt.ylabel("Precision", fontsize=size)
+        plt.savefig("precisions.pdf")
+        plt.clf()
+        plt.boxplot(rs_values)
+        plt.xlabel("Subject", fontsize=size)
+        plt.xticks(
+            list(range(1, len(rs_labels) + 1)),
+            rs_labels,
+            rotation=-30,
+            ha="left",
+            rotation_mode="anchor",
+        )
+        plt.ylabel("Recall", fontsize=size)
+        plt.savefig("recalls.pdf")
+        plt.clf()
 
 
 if __name__ == "__main__":
-    main()
+    main(len(sys.argv) > 1 and sys.argv[1] == "-r")
